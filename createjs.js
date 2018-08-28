da28fe4430a9f3253b45b08eb94358f8c423ed10
Cassidy <cassidyjade623@gmail.com>
2018-08-28 07:04:38 -0400

    just testing navigation/ adding placeholders

--- createjs.js
+++ createjs.js
@@ -17,3 +17,19 @@ function showPanel(panelIndex, colorCode){
 }
 showPanel(1,'blue');
 showPanel(0,'blue');
+// testing out routing to other pages. I can make a new file for this .js code if it agrees with common standards to do so
+var  menbar = angular.module('menbar', ['ngRoute']);
+
+menbar.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+  .when('/index', {
+    templateUrl: '/index.html'
+  })
+  .when('/create', {
+    templateUrl: '/create.html'
+  })
+  .otherwise({
+    redirectTo: '/index'
+  });
+
+}]);
