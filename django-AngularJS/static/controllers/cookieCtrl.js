4e159e82cc23234b0efe8f7ea7029f7c615ff52c
annavaykean <42591977+annavaykean@users.noreply.github.com>
2018-10-03 20:11:25 -0400

    fixing label and added topnav to cookie page

--- django-AngularJS/static/controllers/cookieCtrl.js
+++ django-AngularJS/static/controllers/cookieCtrl.js
@@ -6,4 +6,20 @@ angular.module('myApp', ['ngMaterial']).controller('cookieCtrl', function($scope
         $scope.p1score++;
     }
 
+    $scope.goto = function(dest){
+        if(dest == 'home'){
+            window.location.href = "/static/templates/cookie.html";
+        }
+        else if(dest == 'leaderboard'){
+            window.location.href="/static/templates/leaderboard.html";
+        }
+        else if(dest == 'logout'){
+            //code to end user session here
+            window.location.href="/static/templates/index.html";
+        }
+        else
+            console.log("That shouldn't work");
+    
+    }
+
 });
