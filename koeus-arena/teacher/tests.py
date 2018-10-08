3131ee9ad2456c70f60619c3c87532887e3cbf02
Cassidy <cassidyjade623@gmail.com>
2018-10-07 21:10:59 -0400

    removed cookie testcase since arena handles

--- koeus-arena/teacher/tests.py
+++ koeus-arena/teacher/tests.py
@@ -81,25 +81,25 @@ class RoomViewTests(SimpleTestCase):
         self.assertNotContains(
             response, 'Hi there! I should not be on the page.')
 
-class CookieTests(SimpleTestCase):
-
-    def test_cookie_page_status_code(self):
-        response = self.client.get('/cookie/')
-        self.assertEquals(response.status_code, 200)
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('cookie'))
-        self.assertEquals(response.status_code, 200)
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('cookie'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'registration/cookie.html')
-    def test_cookie_page_contains_correct_html(self):
-        response = self.client.get('/cookie/')
-        self.assertContains(response, '<title>Competition</title>')
-    def test_cookie_page_does_not_contain_incorrect_html(self):
-        response = self.client.get('/cookie/')
-        self.assertNotContains(
-            response, 'Hi there! I should not be on the page.')
+# class CookieTests(SimpleTestCase):
+#
+#     def test_cookie_page_status_code(self):
+#         response = self.client.get('/cookie/')
+#         self.assertEquals(response.status_code, 200)
+#     def test_view_url_by_name(self):
+#         response = self.client.get(reverse('lobby'))
+#         self.assertEquals(response.status_code, 200)
+#     def test_view_uses_correct_template(self):
+#         response = self.client.get(reverse('lobby'))
+#         self.assertEquals(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'registration/cookie.html')
+#     def test_cookie_page_contains_correct_html(self):
+#         response = self.client.get('/cookie/')
+#         self.assertContains(response, '<title>Competition</title>')
+#     def test_cookie_page_does_not_contain_incorrect_html(self):
+#         response = self.client.get('/cookie/')
+#         self.assertNotContains(
+#             response, 'Hi there! I should not be on the page.')
 
 class LeaderBoardTests(SimpleTestCase):
 
