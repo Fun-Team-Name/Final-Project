34ad8e0457cb566b84bc1875c052d89e19dc0ba2
rosscwagner <rosscwagner@gmail.com>
2018-11-11 15:36:44 -0500

    Templates, tests, and nav fixes

--- arena/tests.py
+++ arena/tests.py
@@ -22,15 +22,16 @@ class ArenaTests(ChannelsLiveServerTestCase):
         cls.driver.quit()
         super().tearDownClass()
 
-    def test_when_chat_message_posted_then_seen_by_everyone_in_same_room(self):
+    def test_when_question_answered_then_seen_by_everyone_in_same_room(self):
         try:
-            self._enter_chat_room('room_1')
+            self._enter_arena('room_1', 'test_1')
 
             self._open_new_window()
-            self._enter_chat_room('room_1')
+            self._enter_arena('room_1', 'test_2')
 
             self._switch_to_window(0)
-            self._post_message('hello')
+            #get sext form question tag
+            self._answer_question()
             WebDriverWait(self.driver, 2).until(lambda _:
                 'hello' in self._chat_log_value,
                 'Message was not received by window 1 from window 1')
@@ -40,13 +41,13 @@ class ArenaTests(ChannelsLiveServerTestCase):
                 'Message was not received by window 2 from window 1')
         finally:
             self._close_all_new_windows()
-'''
+
     def test_when_chat_message_posted_then_not_seen_by_anyone_in_different_room(self):
         try:
-            self._enter_chat_room('room_1')
+            self._enter_arena('room_1', 'test_1')
 
             self._open_new_window()
-            self._enter_chat_room('room_2')
+            self._enter_arena('room_2', 'test_1')
 
             self._switch_to_window(0)
             self._post_message('hello')
@@ -66,9 +67,9 @@ class ArenaTests(ChannelsLiveServerTestCase):
 
     # === Utility ===
 
-    def _enter_chat_room(self, room_name):
-        self.driver.get(self.live_server_url + '/chat/')
-        ActionChains(self.driver).send_keys(room_name + '\n').perform()
+    def _enter_arena(self, room_name, alias):
+        self.driver.get(self.live_server_url + '/arena/')
+        ActionChains(self.driver).send_keys(room_name + '\t'+alias+'\n').perform()
         WebDriverWait(self.driver, 2).until(lambda _:
             room_name in self.driver.current_url)
 
@@ -85,11 +86,17 @@ class ArenaTests(ChannelsLiveServerTestCase):
 
     def _switch_to_window(self, window_index):
         self.driver.switch_to_window(self.driver.window_handles[window_index])
-
+    '''
     def _post_message(self, message):
         ActionChains(self.driver).send_keys(message + '\n').perform()
+    '''
+    def _answer_question(self):
+        element = self.driver.find_element_by_id('question')
+        str = element.text
+        nums = [int(s) for s in str.split() if s.isdigit()]
+        answer = str(nums[0]*nums[1])
+        ActionChains(self.driver).send_keys(answer + '\n').perform()
 
     @property
     def _chat_log_value(self):
         return self.driver.find_element_by_css_selector('#chat-log').get_property('value')
-'''
